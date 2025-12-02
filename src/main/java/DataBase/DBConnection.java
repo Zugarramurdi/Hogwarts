7acecf06aa33dd2e6fc407ad4b8fb1ab14e929be
Zugarramurdi <juanma.segura.canales@gmail.com>
2025-12-02 18:45:15 +0100

    Cambiado metodo de conexion de DBConnection con Properties

--- src/main/java/DataBase/DBConnection.java
+++ src/main/java/DataBase/DBConnection.java
@@ -1,26 +1,42 @@
 package DataBase;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/hp";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Toor";
-
-    public static Connection getConnection() {
-
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println("✅✅✅ Connected to Hogwarts!.");
-        } catch (SQLException e) {
-            System.err.println("❌❌❌ Connection error: " + e.getMessage());
-
+        private static Properties loadProperties(){
+            Properties prop = new Properties();
+            try(InputStream in = Files.newInputStream(Paths.get("config/db.properties"))){
+                prop.load(in);
+            } catch (IOException e){
+                System.err.println("Nose ha podido leer el fichero properties "+e.getMessage());
+            }
+            return prop;
         }
-    return conn;
-    }
+
+        public static Connection getConnection() {
+            try {
+                Properties prop = loadProperties();
+
+                String url = prop.getProperty("db.url");
+                String user = prop.getProperty("db.user");
+                String password = prop.getProperty("db.password");
+
+                Connection conn = DriverManager.getConnection(url,user,password);
+                System.out.println("✅✅✅ Connected to Hogwarts!");
+
+                return conn;
+
+            } catch (SQLException e) {
+                throw new RuntimeException("❌❌❌ No se admiten Muggles ",e);
+            }
+        }
 
 }
